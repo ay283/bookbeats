6613ce23420e3d6c244e9029f566209c998d8940
Stuti <sg2265@cornell.edu>
2024-04-26 01:04:59 -0400

    added comments for readability

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,6 @@ import os
 import re
 from flask import Flask, render_template, request
 from flask_cors import CORS
-# from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import normalize
 import pandas as pd
@@ -15,42 +14,32 @@ import requests
 from datetime import datetime, timedelta
 from flask import jsonify
 
-# ROOT_PATH for linking with all your files. 
-# Feel free to use a config.py or settings.py with a global export variable
+# Set the root path for the application, useful for file linking
 os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 
-# Get the directory of the current script
+# Get the directory where the current script is located
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Specify the path to the JSON file relative to the current script, 
-#assumming data is stored in 'GoodReads_condensed.json' and 'Spotify_condensed.json'
-
+# Define paths to the JSON files containing the datasets for GoodReads and Spotify
 goodreads_file_path = os.path.join(current_directory, 'GoodReads_condensed.json')
-
 spotify_file_path = os.path.join(current_directory, 'Spotify_condensed.json')
 
-
+# Load GoodReads data from JSON and convert it to a pandas DataFrame
 with open(goodreads_file_path, 'r') as file:
     book_data = json.load(file)
-    # Convert JSON to DataFrame
     book_df = pd.DataFrame.from_dict(book_data)
-    # book_df.reset_index(inplace=True)
-    # book_df.rename(columns={'index': 'id'}, inplace=True)
-    # book_df = pd.DataFrame(book_data)
 
+# Load Spotify data from JSON and convert it to a pandas DataFrame
 with open(spotify_file_path, 'r') as file:
     spotify_data = json.load(file)
-    # Convert JSON to DataFrame
     spotify_df = pd.DataFrame.from_dict(spotify_data)
-    # spotify_df.reset_index(inplace=True)
-    # spotify_df.rename(columns={'index': 'id'}, inplace=True)
-    # spotify_df = pd.DataFrame(spotify_data)
 
+# Initialize Flask app with configurations for template reloading and CORS
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 CORS(app)
 
-#Manual mapping of book genres to song genres (First prototype only)
+# Mapping of book genres to song genres for initial matching of interests
 book_to_song_genres = {'Nonfiction': ['folk', ' indie'], 'Fiction': [' indie', ' pop', 'pop'],
 'Fantasy': [' pop', 'electronic', 'pop', ' indie'], 'Romance': [' pop', ' 90s'], 'Historical': [' classic rock', ' blues'],
 'History': [' classic rock', ' blues'], 'Childrens': [' pop',  'pop'], 'Cultural': [' 90s', ' 70s'], 
@@ -58,28 +47,25 @@ book_to_song_genres = {'Nonfiction': ['folk', ' indie'], 'Fiction': [' indie', '
 'Science': ['electronic', 'rock'], 'Science Fiction': [' pop', ' indie rock', 'electronic'], 
 'Paranormal': [' classic rock', 'gothic metal', 'hard rock']}
 
+# Function to find the closest related projects using cosine similarity
 def closest_projects(project_index_in, project_repr_in, documents, k = 10):
     sims = project_repr_in.dot(project_repr_in[project_index_in,:])
     asort = np.argsort(-sims)[:k+1]
     return [(documents[i][0], documents[i][1], documents[i][2],documents[i][4], sims[i]) for i in asort[1:]]
     
-#FOR TESTING PURPOSES!: 
-# cosine similarity
 def closest_words(word_in, words_representation_in, k = 10):
     if word_in not in word_to_index: return "Not in vocab."
     sims = words_representation_in.dot(words_representation_in[word_to_index[word_in],:])
     asort = np.argsort(-sims)[:k+1]
     return [(index_to_word[i],sims[i]) for i in asort[1:]] 
 
-#Performing SVD CALCULATION on filtered_spotify dataframe + our query!! 
-#Return the docs_compressed matrix and docs to perform cossine sim on later!
+# Perform SVD on Spotify data filtered by the book's genres and add our book query for comparison
 def svd_calculation(query, filtered_df):
-    # #Terms = Query terms OR terms = all terms of filtered_df 
+    # Convert filtered DataFrame to JSON and write to a file for processing
     print("start td matrix") 
-    # #Tuple list representation of our filtered_df:
     json_filtered = filtered_df.to_json(orient= "records")
     
-        # Write JSON string to a file
+    # Write JSON string to a file
     with open("filtered_data.json", "w") as f:
         f.write(json_filtered)
 
@@ -92,16 +78,10 @@ def svd_calculation(query, filtered_df):
                 for x in data
                 if len(x['text'].split()) > 50]
     
-    # documents = [(x['title'], x['artist'], x['spotify_id'], x['text'])
-    #              for _, x in filtered_df.iterrows()
-    #              if len(x['text'].split()) > 50]
-    
-    #Ensure that query label will be the last entry in 'docs_compressed_normed' 
+    # Ensure that query label will be the last entry in 'docs_compressed_normed' 
     process = [x[3] for x in documents]
-    process.append(query) #ORder preserved!
+    process.append(query) # Add query at the end to preserve order
     print("process")
-    # for pair in process:
-    #     print(pair, "words")
     vectorizer = CountVectorizer()
     td_matrix = vectorizer.fit_transform(process)
     td_matrix=td_matrix.astype('float64')
@@ -110,8 +90,7 @@ def svd_calculation(query, filtered_df):
     
     return docs_compressed_normed, documents
 
-#Filtering spotify data based on book genre
-#query = book of interest
+# Filter Spotify data to find songs related to the book's genres
 def song_filter(query):
     book = book_df[(book_df['title'] == query)]
     our_genres = book['genretokenized']
@@ -119,53 +98,43 @@ def song_filter(query):
     for key, s_gen in book_to_song_genres.items():
         for genre in our_genres:
             for genre1 in genre:
-                #print(key, 'key',genre1, 'genre')
                 if key == genre1:
-                    #print("equal keys", key, genre1)
                     rel_song_genre_set.update(s_gen)
     if (len(rel_song_genre_set)==0):
-        #print("HERE")
         return spotify_df
     rel_song_genre_list = list(rel_song_genre_set)
     print(rel_song_genre_list)
     filtered_df = spotify_df[spotify_df['tagstokenized'].apply(lambda x: any(genre in x for genre in rel_song_genre_list))]
-    #print(filtered_df.shape[0])
     return filtered_df
 
-# @app.route("/")
-# def home():
-#     return render_template('base.html',title="sample html")
 @app.route("/")
 def home():
-    # Pass all book titles to the frontend
+    # Pass all book titles to the frontend for user interaction
     all_titles = book_df['title'].tolist()
     return render_template('base.html', title="Sample HTML", all_titles=jsonify(all_titles))
 
-#Route to return top ten similar songs to a book (based on cossine similarity)
+# Route to return top ten similar songs to a book (based on cossine similarity)
 @app.route("/songs", methods = ['GET'])
 def episodes_search():
-    #popular_genres()
     text = request.args.get("title")
     book = book_df[(book_df['title'] == text)]
     if book.empty:
         print("DNE")
         return json.dumps({"error": "Book Not Found"}), 404
    
-    #Filter our spotify_df as needed:
+    # Filter our spotify_df as needed:
     filtered_spotify = song_filter(text)
     song_count = filtered_spotify.shape[0]
     print(song_count, "size of filtered")
 
-    #Perform SVD on Filtered DF 
+    # Perform SVD on Filtered DF 
     desc = return_desc(text)
     print(desc)
     docs_compressed_norm, documents = svd_calculation(desc,filtered_spotify)
-    #print("HERE")
-    #print(docs_compressed_norm[song_count])
-    #Provides top ten cossine sim 
+    # Provides top ten cossine sim 
     top_ten_songs = closest_projects(song_count, docs_compressed_norm, documents) #Returns tuple list 
     
-    #Response json
+    # Response json
     response_json = {}
     response_json["top_ten_songs"] = []  
     for title, artist, id, genres, sim in top_ten_songs:
@@ -174,10 +143,9 @@ def episodes_search():
         print(title, artist, id, genres, sim)
     return json.dumps(response_json), 200
 
+# Spotify API credentials management
 CLIENT_ID = '1124360dd48e4ace9c3be693c3f5f764'
 CLIENT_SECRET = '51412712cb8742958a0282f924de6e50'
-
-# Token information
 token_info = {
     'access_token': None,
     'expires_at': None
@@ -240,10 +208,6 @@ def fetch_book_title_suggestions(query):
     sorted_titles = sorted(filtered_titles)
     return sorted_titles
 
-
-
-
-
 def get_song_details(track_id):
     token = get_token()
     headers = {
@@ -265,32 +229,8 @@ def get_song_details(track_id):
         return song_details
     else:
         return f"Failed to retrieve song details: {response.status_code}"
-    
-print(get_song_details("09ZQ5TmUG8TSL56n0knqrj"))
 
-#____________________EXTRA FUNCTIONS FROM P03 (MAY OR MAY NOT USE LATER)______________________________________________#
-
-#Get most popular song genres and most popular book genres in our datasets (helping to map genres)
-def popular_genres():
-    #Unionize the songs
-    song_genres_count = {}
-    for genre_list in spotify_df['tagstokenized']:
-        for genre in genre_list:
-            old_count = song_genres_count.get(genre, 0)
-            song_genres_count[genre] = old_count+1
-    book_genres_count = {}
-    for genre_list in book_df['genretokenized']:
-        for genre in genre_list:
-            old_count = book_genres_count.get(genre, 0)
-            book_genres_count[genre] = old_count+1
-    sorted_song_genre_count = (sorted(song_genres_count.items(), key=lambda x: x[1], reverse= True))
-    sorted_book_genres_count = (sorted(book_genres_count.items(), key=lambda x: x[1], reverse= True))
-    for pair in sorted_book_genres_count[:20]:
-            print(pair)
-    for pair in sorted_song_genre_count[:20]:
-            print(pair)
-
-#Return the title + description of our book 
+# Helper function to return a formatted description of a book
 def return_desc(query):
     filtered_df = book_df[(book_df['title'] == query)]
     #tokenize title
@@ -301,49 +241,9 @@ def return_desc(query):
     desc_str = filtered_df['desc'].iloc[0]
     res = title_str + " " + desc_str
     return res
-    
-#Tokenize the title and description of a book based on query title given
-def tokenize_book(query):
-    filtered_df = book_df[(book_df['title'] == query)]
-    #tokenize title
-    if filtered_df.empty:
-        print("Book does not exist in our database", query)
-        return []
-    title_str = filtered_df['title'].iloc[0]
-    title_tok = re.findall(r'\w+\'?\w*', title_str)
-    desc_str = filtered_df['desc'].iloc[0]
-    desc_tok = re.findall(r'\w+\'?\w*', desc_str)
-    res = title_tok + desc_tok
-    return desc_str
 
-#Tokenize the title and text of a song by row id given
-def tokenize_song_by_i(i, filtered_spotify):
-    title_str = filtered_spotify['title'].iloc[i]
-    title_tok = re.findall(r'\w+\'?\w*', title_str)
-    text_str = filtered_spotify['text'].iloc[i]
-    text_tok = re.findall(r'\w+\'?\w*', text_str)
-    res = title_tok + text_tok
-    return res
-
-#Builds Frequency Vector
-
-def build_vector(doc, query_words):
-    
-    frequency_vector= [0] * len(query_words)
-    
-    # Create a dictionary to store the index of each word in the query vocabulary
-    word_index = {word: i for i, word in enumerate(query_words)}
-
-    # Update the counts in the frequency vector for words present in both doc and query_words
-    for word in doc:
-        if word in word_index:
-            index = word_index[word]
-            frequency_vector[index] += 1
-
-    return frequency_vector
-
+# Calculate cosine similarity, handling edge cases such as division by zero
 def cossim(a, b):
-  #  print(a,b)
     if np.isnan(norm(b)) or np.isnan(norm(a)) or norm(a) == 0 or norm(b) ==0 or not a or not b:
         return 0
     ans = dot(a, b) / (norm(a) * norm(b))
